01e324768e83b1b27ad7f6e03bc1c70809ec997e
Khanh Duong <dqkqdlot@gmail.com>
2025-01-02 21:21:40 +0900

    Week1 day2 task2

--- mini-lsm-starter/src/iterators/merge_iterator.rs
+++ mini-lsm-starter/src/iterators/merge_iterator.rs
@@ -1,10 +1,7 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use std::cmp::{self};
 use std::collections::BinaryHeap;
 
-use anyhow::Result;
+use anyhow::{Context, Result};
 
 use crate::key::KeySlice;
 
@@ -45,7 +42,27 @@ pub struct MergeIterator<I: StorageIterator> {
 
 impl<I: StorageIterator> MergeIterator<I> {
     pub fn create(iters: Vec<Box<I>>) -> Self {
-        unimplemented!()
+        let mut iters: BinaryHeap<HeapWrapper<I>> = iters
+            .into_iter()
+            // Filter out invalid iterator.
+            .filter(|iter| iter.is_valid())
+            .enumerate()
+            .map(|(size, iter)| HeapWrapper(size, iter))
+            .collect();
+
+        let current = iters.pop();
+
+        MergeIterator { iters, current }
+    }
+
+    /// Fetch an iterator with smallest key into `self.current`.
+    fn fetch(&mut self) {
+        while let Some(iter) = self.iters.pop() {
+            if iter.1.is_valid() {
+                self.current = Some(iter);
+                break;
+            }
+        }
     }
 }
 
@@ -55,18 +72,68 @@ impl<I: 'static + for<'a> StorageIterator<KeyType<'a> = KeySlice<'a>>> StorageIt
     type KeyType<'a> = KeySlice<'a>;
 
     fn key(&self) -> KeySlice {
-        unimplemented!()
+        self.current
+            .as_ref()
+            .map(|iter| iter.1.key())
+            .with_context(|| "calling `key()` on invalid merge iterator")
+            .unwrap()
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        self.current
+            .as_ref()
+            .map(|iter| iter.1.value())
+            .with_context(|| "calling `value()` on invalid merge iterator")
+            .unwrap()
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.current.is_some()
     }
 
+    /// Advance to next state.
+    /// Stop if any inner iterator's `next()` return error.
+    ///
+    /// Here are the steps:
+    /// - Remove all the keys inside the heap that equals the smallest key.
+    /// - Calling next on `self.current` to discard the smallest key.
+    /// - Push back `self.current` into the heap and get the smallest iterator out.
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        let mut current = self
+            .current
+            .take()
+            .with_context(|| "calling `next()` on invalid merge iterator")?;
+        let smallest_key = current.1.key();
+
+        // Advance until all iterators only contain keys that greater than previous key.
+        while let Some(mut iter) = self.iters.pop() {
+            // Skip invalid iterator
+            if !iter.1.is_valid() {
+                continue;
+            }
+
+            // The smallest key inside the heap is greater than `smallest_key`, can stop.
+            if iter.1.key() > smallest_key {
+                self.iters.push(iter);
+                break;
+            }
+
+            // Advance the pointer and push back iterator if it is valid.
+            iter.1.next()?;
+            if iter.1.is_valid() {
+                self.iters.push(iter);
+            }
+        }
+
+        // Advance `current` to drop `smallest_key`, and push it back to the heap.
+        current.1.next()?;
+        if current.1.is_valid() {
+            self.iters.push(current);
+        }
+
+        // fetch the next iterator out to `self.current`
+        self.fetch();
+
+        Ok(())
     }
 }
