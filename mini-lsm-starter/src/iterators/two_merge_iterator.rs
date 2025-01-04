a02f0380b61785fa5a3314372f8b01043ae9872f
Khanh Duong <dqkqdlot@gmail.com>
2025-01-04 09:59:16 +0900

    Two iterator

--- mini-lsm-starter/src/iterators/two_merge_iterator.rs
+++ mini-lsm-starter/src/iterators/two_merge_iterator.rs
@@ -1,9 +1,12 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use anyhow::Result;
 
 use super::StorageIterator;
+
+#[derive(Debug)]
+enum IteratorState {
+    A,
+    B,
+}
 
 /// Merges two iterators of different types into one. If the two iterators have the same key, only
 /// produce the key once and prefer the entry from A.
@@ -11,6 +14,7 @@ pub struct TwoMergeIterator<A: StorageIterator, B: StorageIterator> {
     a: A,
     b: B,
     // Add fields as need
+    state: IteratorState,
 }
 
 impl<
@@ -19,7 +23,30 @@ impl<
     > TwoMergeIterator<A, B>
 {
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        let mut iter = Self {
+            a,
+            b,
+            state: IteratorState::A,
+        };
+        iter.state = iter.infer_state();
+        Ok(iter)
+    }
+
+    fn infer_state(&self) -> IteratorState {
+        match (self.a.is_valid(), self.b.is_valid()) {
+            (true, true) if self.a.key() <= self.b.key() => IteratorState::A,
+            (true, true) => IteratorState::B,
+            (_, false) => IteratorState::A,
+            (false, _) => IteratorState::B,
+        }
+    }
+
+    /// Skip all the keys in b that equal the key in a
+    fn skip_duplicated_keys(&mut self) -> Result<()> {
+        while self.a.is_valid() && self.b.is_valid() && self.b.key() <= self.a.key() {
+            self.b.next()?;
+        }
+        Ok(())
     }
 }
 
@@ -31,18 +58,30 @@ impl<
     type KeyType<'a> = A::KeyType<'a>;
 
     fn key(&self) -> Self::KeyType<'_> {
-        unimplemented!()
+        match self.state {
+            IteratorState::A => self.a.key(),
+            IteratorState::B => self.b.key(),
+        }
     }
 
     fn value(&self) -> &[u8] {
-        unimplemented!()
+        match self.state {
+            IteratorState::A => self.a.value(),
+            IteratorState::B => self.b.value(),
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.a.is_valid() || self.b.is_valid()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        self.skip_duplicated_keys()?;
+        let _ = match self.state {
+            IteratorState::A => self.a.next(),
+            IteratorState::B => self.b.next(),
+        };
+        self.state = self.infer_state();
+        Ok(())
     }
 }
